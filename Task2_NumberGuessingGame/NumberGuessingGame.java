1636c185f980b5ab3e32bcba846e1a7a7b40cb57
sbolline4-sudo <sbolline4@gitam.in>
2026-06-09 11:59:49 +0530

    Add Number Guessing Game solution

--- Task2_NumberGuessingGame/NumberGuessingGame.java
+++ Task2_NumberGuessingGame/NumberGuessingGame.java
@@ -1,1 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int number = rand.nextInt(100) + 1;
+        int guess = 0;
+        int attempts = 0;
+
+        System.out.println("=== Number Guessing Game ===");
+        System.out.println("Guess a number between 1 and 100");
+
+        while (guess != number) {
+
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+
+            attempts++;
+
+            if (guess > number) {
+                System.out.println("Too High!");
+            }
+            else if (guess < number) {
+                System.out.println("Too Low!");
+            }
+            else {
+                System.out.println("Correct!");
+                System.out.println("Attempts: " + attempts);
+            }
+        }
+
+        sc.close();
+    }
+}
 
